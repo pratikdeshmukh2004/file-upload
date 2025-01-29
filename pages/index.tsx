753d831b810dbf4f0f894be1e558a919c1cdc4ab
Pratik Deshmukh <pratikdeshmukhlobhi@gmail.com>
2025-01-29 14:57:44 +0530

    build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import { useRef } from "react";
 
 export default function Home() {
-  const fileUpload = useRef(null);
   return (
     <div className="text-center">
       <h1 className="text-center text-2xl font-bold mt-20">Upload Photo</h1>
@@ -22,11 +20,10 @@ export default function Home() {
         className="hidden"
         type="file"
         accept="image/*"
-        ref={fileUpload}
         capture
       />
       <button
-        onClick={() => fileUpload.current.click()}
+        onClick={() => document.getElementById("profile")?.click()}
         className="bg-orange-600 w-64 rounded-full p-3 font-bold text-white mt-10"
       >
         Choose a photo
